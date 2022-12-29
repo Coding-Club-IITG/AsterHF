5a6262089ea9246639526febe3b554c7680e8c60
Priyanshu Srivastava <97830682+GeekyPS@users.noreply.github.com>
2022-12-29 23:06:28 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:aster_hf/screens/form_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import './screens/splashscreen.dart';
+import 'package:flutter/services.dart';
+
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -32,6 +34,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ]);
     return ScreenUtilInit(
       minTextAdapt: true,
       designSize: const Size(432, 930),
